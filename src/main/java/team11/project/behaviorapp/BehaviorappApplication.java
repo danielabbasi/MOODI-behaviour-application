3df2094410ec81e4b383521d9876f7508024b19a
Josh Teague <TeagueJ@cardiff.ac.uk>
2017-11-17 10:26:24 +0000

    pushing up intelli j files

---
+++ src/main/java/team11/project/behaviorapp/BehaviorappApplication.java
@@ -0,0 +1,12 @@
+package team11.project.behaviorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BehaviorappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BehaviorappApplication.class, args);
+	}
+}
---
+++ src/main/java/team11/project/behaviorapp/Entities/Patient.java
@@ -0,0 +1,7 @@
+package team11.project.behaviorapp.Entities;
+
+/**
+ * Created by c1673239 on 17/11/2017.
+ */
+public class Patient {
+}
