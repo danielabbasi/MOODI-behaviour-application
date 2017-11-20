6d1c64fc454774670fb1d140e43d54f70a369204
c1673218 <AbbasiD@cardiff.ac.uk>
2017-11-20 21:42:16 +0000

    created getActivityList method in the Service which returns a collection of activities by patient id.

--- src/main/java/team11/project/behaviorapp/Services/PatientService.java
+++ src/main/java/team11/project/behaviorapp/Services/PatientService.java
@@ -3,9 +3,14 @@ package team11.project.behaviorapp.Services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import team11.project.behaviorapp.Entities.Patient;
+import team11.project.behaviorapp.Repositories.ActivityRepository;
+import team11.project.behaviorapp.Repositories.CustomList;
 import team11.project.behaviorapp.Repositories.PatientRepository;
 
+import java.util.Collection;
 import java.util.List;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 /**
  * Created by c1443907 on 17/11/2017.
@@ -16,8 +21,12 @@ public class PatientService {
 
     @Autowired
     PatientRepository patientRepository;
+    @Autowired
+    ActivityRepository activityRepository;
 
     public List<Patient>getAllPatients(){
         return patientRepository.findAll();
     }
+    public Collection<CustomList>getActivityList(Long id) {return activityRepository.findActivitiesById(id);}
+
 }
