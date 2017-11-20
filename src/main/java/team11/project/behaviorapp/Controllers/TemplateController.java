131152327bfaecbd0ac581b383d7cc0919aa05d0
c1673218 <AbbasiD@cardiff.ac.uk>
2017-11-20 21:47:39 +0000

    Used @RequestMapping and @PathVariable to create the path with patient id as a variable. This therefore shows the list of activities depending on what patient id is in the URL. Added the attributes using the getActivityList method in the service. Returns the activityList HTML.

--- src/main/java/team11/project/behaviorapp/Controllers/TemplateController.java
+++ src/main/java/team11/project/behaviorapp/Controllers/TemplateController.java
@@ -3,8 +3,13 @@ package team11.project.behaviorapp.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import team11.project.behaviorapp.Repositories.ActivityRepository;
+import team11.project.behaviorapp.Repositories.CustomList;
 import team11.project.behaviorapp.Services.PatientService;
+
+import java.util.Collection;
 
 /**
  * Created by c1443907 on 17/11/2017.
@@ -15,6 +20,8 @@ public class TemplateController {
 
     @Autowired
     PatientService patientService;
+    @Autowired
+    ActivityRepository activityRepository;
 
     @RequestMapping(path = "/gp/index")
     public String index() {
@@ -33,4 +40,13 @@ public class TemplateController {
     public String createActivity() {
         return "createActivity";
     }
+
+    @RequestMapping("patient/{id}/activityList")
+    public String listActivities(@PathVariable Long id, Model model){
+        model.addAttribute("activities", patientService.getActivityList(id));
+
+        return "activityList";
+    }
+
 }
+
