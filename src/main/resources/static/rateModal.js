6b3b49da296ea63e6e9af881ac24c6f810c911c5
Jamie Highfield <HighfieldJ@cardiff.ac.uk>
2017-11-27 19:57:05 +0000

    Clicking mark as completed now is specific for each activity (parsing the activity ID as a parameter) using Thymeleaf.

--- src/main/resources/static/rateModal.js
+++ src/main/resources/static/rateModal.js
@@ -1,27 +1,16 @@
 //Taken from modal.js and adapted as appropriate
 
-// Get the modal
-var modal = document.getElementById('rate_modal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("rate_modal_button");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
+function showModal(activityId) {
+    document.getElementById('selected_activity_id').value = activityId;
+    document.getElementById('rate_modal').style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+document.getElementsByClassName("close")[0].onclick = function() {
+    document.getElementsByClassName("close")[0].style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        document.getElementsByClassName("close")[0].style.display = "none";
     }
 }
