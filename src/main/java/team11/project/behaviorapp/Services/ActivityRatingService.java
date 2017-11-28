94a08a4a689a2e8a6a58b0fbfabe4198f9c7a4dc
Jamie Highfield <HighfieldJ@cardiff.ac.uk>
2017-11-28 11:01:56 +0000

    Added in relevant rating classes which were excluded from previous commit

---
+++ src/main/java/team11/project/behaviorapp/Services/ActivityRatingService.java
@@ -0,0 +1,20 @@
+package team11.project.behaviorapp.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import team11.project.behaviorapp.Repositories.IActivityRatingRepository;
+
+@Service
+public class ActivityRatingService {
+
+    @Autowired
+    public ActivityRatingService(IActivityRatingRepository activityRatingRepository) {
+        this.activityRatingRepository = activityRatingRepository;
+    }
+
+    private IActivityRatingRepository activityRatingRepository;
+
+    public void rateActivity(long activityId, int rating) {
+        activityRatingRepository.rateActivity(activityId, rating);
+    }
+}
