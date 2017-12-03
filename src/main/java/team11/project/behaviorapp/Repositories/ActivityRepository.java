de2d99f113b9ac3d70db08788a2e30ff1499281a
Ahmed Alsaab <AlsaabA@cardiff.ac.uk>
2017-12-03 18:26:36 +0000

    Implemented a new query that returns the patients last 3 activities and added some business logic to it in the service. This query now populates the patients' index graph through an Ajax request for each unique patientid. Countdown timer has been removed for the time being.

--- src/main/java/team11/project/behaviorapp/Repositories/ActivityRepository.java
+++ src/main/java/team11/project/behaviorapp/Repositories/ActivityRepository.java
@@ -9,6 +9,7 @@ import team11.project.behaviorapp.Entities.Patient;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 public interface ActivityRepository extends JpaRepository<Activities, Long>{
@@ -21,7 +22,7 @@ public interface ActivityRepository extends JpaRepository<Activities, Long>{
     List<Activities> findActivitiesByPatientsAndIsCompletedAndIsDeleted(Patient p, Boolean isCompleted, Boolean isDeleted);
     List<Activities> findActivitiesByPatientsAndIsCompletedOrderByActivityDateAsc(Patient p, Boolean isCompleted);
     List<Activities>findActivitiesByPatientsAndIsFavouriteAndIsDeleted(Patient p, Boolean isFavourite, Boolean isDeleted);
-    List<Activities> findByActivityDateGreaterThan(LocalDateTime aMoment);
+    List<Activities> findFirst3ActivitiesByPatientsAndIsCompletedOrderByActivityDateDesc(Patient p, Boolean isCompleted);
 
     List<Activities> findActivitiesByIsCompleted(Boolean isCompleted);
     Collection<CustomList> findActivitiesByIsDeleted(Boolean x);
@@ -40,6 +41,9 @@ public interface ActivityRepository extends JpaRepository<Activities, Long>{
 
     @Query(value = "SELECT COUNT(name) FROM Activities WHERE isFavourite = 1")
     int getActivitiesByNameAndIsFavourite();
+
+    @Query(value = "SELECT activityDate FROM Activities WHERE id = 1")
+    LocalDateTime getActivitiesByActivityDate();
 
 
 
