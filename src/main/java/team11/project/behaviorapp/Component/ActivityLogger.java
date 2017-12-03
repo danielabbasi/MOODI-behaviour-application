1180add7a618bab0b8ffd61974d1947da9821c6d
c1673218 <AbbasiD@cardiff.ac.uk>
2017-12-03 13:54:59 +0000

    Added @Scheduling and @EnableScheduling. Using scheduling to check whether in the last 10 minutes there has been a new activity inserted into the database. If so then log the patient name that made that activity

---
+++ src/main/java/team11/project/behaviorapp/Component/ActivityLogger.java
@@ -0,0 +1,38 @@
+package team11.project.behaviorapp.Component;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import team11.project.behaviorapp.Entities.Activities;
+import team11.project.behaviorapp.Repositories.ActivityRepository;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+/**
+ * Created by c1673218 on 03/12/2017.
+ */
+@Component
+public class ActivityLogger {
+
+    private ActivityRepository activityRepository;
+
+    @Autowired
+    public ActivityLogger(ActivityRepository aRepo) {
+        activityRepository = aRepo;
+    }
+
+    @Scheduled(fixedRate = 30000)
+    @Transactional
+    public void logActivityReminder(){
+        LocalDateTime start = LocalDateTime.now().minus(10, ChronoUnit.MINUTES);
+        System.out.println("Activities Since " + start);
+        for (Activities a : activityRepository.findByActivityDateGreaterThan(start) ){
+            System.out.println(a.getName());
+        }
+    }
+
+}
