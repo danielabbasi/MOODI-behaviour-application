68c71bcbb20a0b616278d2842e09f64d7a730313
c1673218 <AbbasiD@cardiff.ac.uk>
2017-12-04 17:13:51 +0000

    Created app.js to deal with the web sockets which has a function that appends the HTML with data it has been sent from the logger

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,46 @@
+/**
+ * Created by c1673218 on 04/12/2017.
+ */
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html();
+}
+
+function connect() {
+    var socket = new SockJS('/gs-guide-ActivityNotifier');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('CONNECTED TO FRAME : ' + frame);
+        stompClient.subscribe('/topic/greetings', function (message) {
+            showGreeting(message);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message.body + "</td></tr>");
+}
+
+$(document).ready(function () {
+    connect(); // this works
+    console.log("CONNNECTED");
+});
