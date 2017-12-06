41ce25c722a0e6b14761a44e84a2e3e6a3aba097
Ahmed Alsaab <AlsaabA@cardiff.ac.uk>
2017-12-06 17:18:01 +0000

    Adapted the code for the rating-after and placed the slider in the modal popup for notifications - Pending changes

--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -37,6 +37,34 @@ function markActivityAsComplete() {
     httpRequest.send();
 }
 
+function sendRatingBefore() {
+    var completeButton = document.getElementById("complete_button");
+    completeButton.innerHTML = "Sending Score (please wait)...";
+    completeButton.disabled = true;
+    var activityId = document.getElementById("greetings").value;
+    var testResult = document.getElementById("test-result").innerHTML.toLowerCase();
+    var testResultNumber = 0;
+    if(testResult == "very happy") {
+        testResultNumber = 1;
+    } else if(testResult == "happy") {
+        testResultNumber = 2;
+    } else if(testResult == "neutral") {
+        testResultNumber = 3;
+    } else if(testResult == "sad") {
+        testResultNumber = 4;
+    } else if(testResult == "upset") {
+        testResultNumber = 5;
+    } else {
+        testResultNumber = 6;
+    }
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        window.setTimeout(refreshPage,1000);
+    }
+    httpRequest.open("POST", "/api/patient/activities/" + activityId + "/rate?ratingAfter=" + testResultNumber)
+    httpRequest.send();
+}
+
 function deleteActivity() {
     var completeButton = document.getElementById("delete_button");
     completeButton.innerHTML = "Delete Activity (please wait)...";
