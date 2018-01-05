d996039447e8021600b5501da46cdd785457d4e5
c1673218 <AbbasiD@cardiff.ac.uk>
2018-01-05 15:51:07 +0000

    Added some new tests

--- src/test/java/team11/project/behaviorapp/BehaviorappApplicationTests.java
+++ src/test/java/team11/project/behaviorapp/BehaviorappApplicationTests.java
@@ -1,16 +1,16 @@
-//package team11.project.behaviorapp;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class BehaviorappApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//	}
-//
-//}
+package team11.project.behaviorapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BehaviorappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
